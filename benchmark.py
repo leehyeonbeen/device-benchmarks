a775ab3ad6f8f35de7775c50050f2b4c0768c4ed
Edward Hyeonbeen Lee <edward.hyeonbeen.lee@gmail.com>
2025-07-18 12:37:08 +0900

    modify for myself

--- benchmark.py
+++ benchmark.py
@@ -2,6 +2,7 @@ import torch
 import time
 import argparse
 import numpy as np
+
 try:
     import intel_extension_for_pytorch as ipex
     from torch import xpu
@@ -10,45 +11,59 @@ except ImportError:
 from torch import mps, cuda
 
 
-
-parser = argparse.ArgumentParser(description='Measure FLOPs and BW.')
-parser.add_argument('--device', type=str, default='cpu',
-                    help='One of cpu | cuda | mps | xpu')
-parser.add_argument('--num_trails', type=int, default=10,
-                    help='Number of trails to get average.')
-parser.add_argument('--dtype', type=str, default='float',
-                    help='One of float32|float64|float16|bfloat16|int8|int16|int32|bool')
+parser = argparse.ArgumentParser(description="Measure FLOPs and BW.")
+parser.add_argument(
+    "--device", type=str, default="cpu", help="One of cpu | cuda | mps | xpu"
+)
+parser.add_argument(
+    "--num_trails", type=int, default=100, help="Number of trails to get average."
+)
+parser.add_argument(
+    "--dtype",
+    type=str,
+    default="float",
+    help="One of float32|float64|float16|bfloat16|int8|int16|int32|bool",
+)
 args = parser.parse_args()
 
 dtype = getattr(torch, args.dtype)
 device = torch.device(args.device)
 num_trails = args.num_trails
+eps = 1e-6
 
 
 def flops_benchmark(device):
-    test_range = 2 ** np.arange(8, 13, 0.25)
+    print(f'Device "{device}" {dtype} matmul compute benchmark: ')
 
-    print(f'benchmarking {device} using {dtype}')
-    print('size, elapsed_time, tops')
+    tflops_log = []
+    test_range = 2 ** np.arange(11, 13 + eps, 0.25)  # 64~4096
     for n in test_range:
-        total = 0
+        n = int(n)
+        a = 10 * torch.rand(n, n, device=device)
+        a = a.to(dtype)
+
+        # warmup
+        for _ in range(3):
+            torch.matmul(a, a)
+
+        t = 0
         for _ in range(num_trails):
-            n = int(n)
-            a = 10 * torch.rand(n, n, device=device)
-            a = a.to(dtype)
-
             synchronize(device)
-            now = time.time()
-            b = torch.matmul(a, a)
+            now = time.perf_counter()
+            torch.matmul(a, a)
             synchronize(device)
+            t += time.perf_counter() - now
 
-            total += time.time() - now
+        t /= num_trails
 
-        total = total / num_trails
+        tflops = 2 * n**3 / t / 1e12
+        tflops_log.append(tflops)
 
-        tflops = 2 * n**3 / total / 1e12
-
-        print(n, total, tflops, sep=", ")
+        # print(n, t, tflops, sep=", ")
+        print(f"Tensor size {n:,}, Elapsed time {t:.4e}s, {tflops:.4f} TOPs.")
+    print(
+        f'Device "{device}" yields {np.mean(tflops_log).item():.4f} TOPs on {dtype} (min: {np.min(tflops_log).item():.4f} TOPs, max: {np.max(tflops_log).item():.4f} TOPs)\n'
+    )
 
 
 def synchronize(device):
@@ -63,9 +78,10 @@ def synchronize(device):
 
 
 def memory_bandwidth_benchmark(device):
-    test_range = 2 ** (np.arange(20, 27, 0.5))
+    print(f'Device "{device}" memory bandwidth benchmark: ')
 
-    print('size (GB), elapsed_time, bandwidth (GB/s)')
+    bandwidth_log = []
+    test_range = 2 ** (np.arange(24, 30 + eps, 1))
     for size in test_range:
         elapsed_time = 0
         for _ in range(num_trails):
@@ -81,7 +97,7 @@ def memory_bandwidth_benchmark(device):
             synchronize(device)
 
             # Record the start time
-            start_time = time.time()
+            start_time = time.perf_counter()
 
             # Perform the copy operation
             a.copy_(b)
@@ -90,7 +106,7 @@ def memory_bandwidth_benchmark(device):
             synchronize(device)
 
             # Record the end time
-            end_time = time.time()
+            end_time = time.perf_counter()
 
             # Compute elapsed time
             elapsed_time += end_time - start_time
@@ -99,9 +115,13 @@ def memory_bandwidth_benchmark(device):
         # Calculate Bandwidth in GB/s
         bytes_copied = a.nelement() * a.element_size()  # bytes
         bandwidth = 2 * bytes_copied / elapsed_time / 1e9  # GB/s
-
-        print(bytes_copied / 1e9, elapsed_time, bandwidth, sep=', ')
-
+        bandwidth_log.append(bandwidth)
+        print(
+            f"Copy size {bytes_copied/1e9:.4f} GB, Elapsed time {elapsed_time:.4e}s, Bandwidth {bandwidth:.4f} GB/s"
+        )
+    print(
+        f'Device "{device}" yields {np.mean(bandwidth_log).item():.4f} GB/s (min: {np.min(bandwidth_log).item():.4f} GB/s, max: {np.max(bandwidth_log).item():.4f} GB/s)\n'
+    )
     return bandwidth
 
 
